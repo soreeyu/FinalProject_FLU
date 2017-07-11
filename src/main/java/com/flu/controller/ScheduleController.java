9c8ee15827d4cac7b1f68121007576e5787a862e
user2-PC\user2 <xkqqkralgus@naver.com>
2017-07-11 19:17:51 +0900

    대략적인 mapper세팅

--- src/main/java/com/flu/controller/ScheduleController.java
+++ src/main/java/com/flu/controller/ScheduleController.java
@@ -1,20 +1,14 @@
 package com.flu.controller;
 
-import java.util.List;
-
-import javax.swing.plaf.synth.SynthSeparatorUI;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.flu.member.MemberDTO;
 import com.flu.schedule.ScheduleService;
 import com.flu.schedule.client.ScheduleMainDTO;
 import com.flu.schedule.client.SchedulePartArrayDTO;
-import com.flu.schedule.client.SchedulePartDTO;
 import com.flu.schedule.client.ScheduleUnitDTO;
 
 @Controller
@@ -23,56 +17,22 @@ public class ScheduleController {
 	
 		@Autowired
 		private ScheduleService scheduleService;
-	
-	   //참여하고 있는 프리랜서 목록 가져오기 
-		public void userList(int projectNum){ //재식친구의 DTO 리스트
-			//List<MemberDTO> 
-		} 
-		
-		public void userOne(){}
 		
 		
-		
-		
-		
-		
-		
-		
-		//저장된 part들 가져오기 //세부사항 등록시 필요
-		public void partList(int scheduleNum){
-			//List<SchedulePartDTO>
-		} 
-		
-		public void partOne(){}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//할일 리스트 뿌려주기 
-		public void unitList(int scheduleNum){ 
-			//List<ScheduleUnitDTO>
+		// 프로젝트에 생성된 스케줄이 있는지 확인
+		@RequestMapping(value="checkS", method=RequestMethod.GET)
+		public void checkSchedule(Integer projectNum){
+			//있으면 바로 스케줄메인화면
+			//없으면
+			//시작날짜,마감날짜,파트 입력화면
+			
+			
 		}
 		
-		public void unitOne(){}
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		//make Schedule1 //애초에 이 작업이 제대로 진행안되면 스케줄이 아예 생성이 안된다고 보면됨
 		@RequestMapping(value="create", method=RequestMethod.GET)
-		public String insertMainSchedule(Integer projectNum, Model model){ //넘어온 projectNum 이 저장되어있다 
+		public String createSchedule(Integer projectNum, Model model){ //넘어온 projectNum 이 저장되어있다 
 			model.addAttribute("projectNum", projectNum);
 			return "schedule/mainInsertForm";
 		}
@@ -93,19 +53,8 @@ public class ScheduleController {
 		}
 
 		
-		//make Schedule2 //같은 view에서 받아온 것들 //스케줄 생성이 성공하면 실행된다
-		@RequestMapping(value="addPart", method=RequestMethod.POST)
-		public String insertPart(SchedulePartArrayDTO schedulePartArrayDTO){
-			int result =  scheduleService.insertPart(schedulePartArrayDTO);
-			
-			System.out.println("part등록 Controller result = "+result);
-			
-			return "schedule/main";
-		}
 		
-		
-		
-		
+
 		
 		//main스케줄 수정하기 //시작 날짜, 마감날짜가 바뀐다 
 		public int updateMainSchedule(ScheduleMainDTO scheduleMainDTO){
@@ -114,6 +63,89 @@ public class ScheduleController {
 		
 		
 		
+		
+		
+
+		
+		
+		//저장된 part들 가져오기 //세부사항 등록시 필요
+		public void partList(int scheduleNum){
+			//List<SchedulePartDTO>
+		} 
+		
+		public void partOne(){}
+		
+		
+		//DTO내의 배열에 각각 값이 저장됨
+		@RequestMapping(value="addPart", method=RequestMethod.POST)
+		public String partWrite(SchedulePartArrayDTO schedulePartArrayDTO){
+			int result =  scheduleService.insertPart(schedulePartArrayDTO);
+
+			System.out.println("part등록 Controller result = "+result);
+
+			return "schedule/main";
+		}
+		
+		public void partUpdate(){}
+		
+		public void partDelete(){}
+		
+		
+		
+		
+		
+		
+		
+		//할일 리스트 뿌려주기 
+		public void unitList(int scheduleNum){ 
+			//List<ScheduleUnitDTO>
+		}
+		
+		public void unitOne(){}
+		
+		
+		public void unitWrite(){}
+		
+		public void unitUpdate(){}
+		
+		public void unitDelete(){}
+		
+		
+		
+
+		//참여하고 있는 프리랜서 목록 가져오기 
+		public void userList(int projectNum){ //재식친구의 DTO 리스트
+			//List<MemberDTO> 
+		} 
+
+		public void userOne(){}
+
+		public void userWrite(){}
+
+		public void userUpdate(){}
+
+		public void userDelete(){}
+
+
+
+		
+		
+
+		
+		//프리랜서 용 // 할일에 지정되있는 user가 email 계정과 동일해야한다  
+		public void stateChange(String user,Integer doNum){
+			//clientSchedule 부분에 state를 변경해준다 
+		}	
+		
+		//프리랜서 용 // 할일에 지정되있는 user가 email 계정과 동일해야한다  
+		public void uploadResult(String file,String contents){ //파일이 여러개일 수 있음 //멀티파트파일리스트를 받아야할수도 있음
+			
+		}  
+		
+		
+		
+		
+
 		
 		//part수정하기 //진행중에 part 수정하는 경우에는 DB에서 part와 clientSchedule 의 part도 같이 수정되어야한다
 		public int updatePart(ScheduleMainDTO scheduleMainDTO){
@@ -140,16 +172,6 @@ public class ScheduleController {
 		
 
 
-		
-		//프리랜서 용 // 할일에 지정되있는 user가 email 계정과 동일해야한다  
-		public void stateChange(String user,Integer doNum){
-			//clientSchedule 부분에 state를 변경해준다 
-		}	
-		
-		//프리랜서 용 // 할일에 지정되있는 user가 email 계정과 동일해야한다  
-		public void uploadResult(String file,String contents){ //파일이 여러개일 수 있음 //멀티파트파일리스트를 받아야할수도 있음
-			
-		}  
 		
 	
 	
--- src/main/java/com/flu/schedule/freelancer/SheduleFreeTodoResultDTO.java
+++ src/main/java/com/flu/schedule/freelancer/SheduleFreeTodoResultDTO.java
@@ -4,8 +4,8 @@ public class SheduleFreeTodoResultDTO {
 	
 	//scheduleNum, doNum 으로 ClientSchedule(UnitDTO) 에 접근가능함
 	private int scheduleNum;
-	private int doNum;
-	private int doResultFileNum;//고유번호로 생성되야함
+	private int unitNum;
+	private int unitResultFileNum;//고유번호로 생성되야함
 	private String email;
 	//여기에 그냥 파일 리스트를 넣어도 좋을 듯 하다 //ScheduleResultFileDTO 대신
 	
@@ -15,17 +15,18 @@ public class SheduleFreeTodoResultDTO {
 	public void setScheduleNum(int scheduleNum) {
 		this.scheduleNum = scheduleNum;
 	}
-	public int getDoNum() {
-		return doNum;
+	
+	public int getUnitNum() {
+		return unitNum;
 	}
-	public void setDoNum(int doNum) {
-		this.doNum = doNum;
+	public void setUnitNum(int unitNum) {
+		this.unitNum = unitNum;
 	}
-	public int getDoResultFileNum() {
-		return doResultFileNum;
+	public int getUnitResultFileNum() {
+		return unitResultFileNum;
 	}
-	public void setDoResultFileNum(int doResultFileNum) {
-		this.doResultFileNum = doResultFileNum;
+	public void setUnitResultFileNum(int unitResultFileNum) {
+		this.unitResultFileNum = unitResultFileNum;
 	}
 	public String getEmail() {
 		return email;
