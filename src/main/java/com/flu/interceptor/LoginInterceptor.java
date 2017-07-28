7eee8dcd6c945bf0f3c3846e2c812376edeb7942
jaesik-PC\jaesik <jk930@naver.com>
2017-07-28 20:08:31 +0900

    로그인 인터셉터 추가

---
+++ src/main/java/com/flu/interceptor/LoginInterceptor.java
@@ -0,0 +1,27 @@
+package com.flu.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginInterceptor extends HandlerInterceptorAdapter{
+
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		try{
+			if(request.getSession().getAttribute("member") == null){
+				response.sendRedirect("/flu/member/login");
+				return false;
+			}	
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return true;
+	}
+}
